cc9270226aba68dc6d9a2dc0f964d0b962b19673
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2017-10-17 09:29:17 -0500

    updated tours route to include artistsand fixed issues in the tours views index page

--- routes/tours.js
+++ routes/tours.js
@@ -14,8 +14,8 @@ router.get('/', (req, res) => {
   .orderBy('tours_murals.tour_id', 'asc')
   .orderBy('tours_murals.tour_stop', 'asc')
   .then((toursMurals) => {
-    res.send(toursMurals);
-    //res.render('tours/index', {toursMurals});
+    //res.send(toursMurals);
+    res.render('tours/index', {toursMurals});
   })
 });
 
