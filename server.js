887adf8c14c8084426b97c2d204a753a7ebf81f9
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-17 10:49:32 -0500

    feature: adds authentication endpoint, stores email and hashed pw in users table

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 app.use(bodyParser.json());
 
@@ -12,21 +13,24 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 
-const murals = require('./routes/murals')
-const neighborhoods = require('./routes/neighborhoods')
-const artists = require('./routes/artists')
-const tours = require('./routes/tours')
+const murals = require('./routes/murals');
+const neighborhoods = require('./routes/neighborhoods');
+const artists = require('./routes/artists');
+const tours = require('./routes/tours');
+const users = require('./routes/users');
+
 
 app.use('/murals', murals);
 app.use('/neighborhoods', neighborhoods);
 app.use('/artists', artists);
 app.use('/tours', tours);
+app.use('/users', users);
 
 
 // TODO - delete after deploying to production
 app.get('/', (req, res, next) => {
   res.send('hello world');
-})
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
